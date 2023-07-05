0e1ab6245f534b7cf76e3458473ba5260b3ece1a
Alex Cooper <616126+bondarev@users.noreply.github.com>
2023-07-05 21:04:16 +0400

    Add problem 3.1 solution `(create-getter)`

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,66 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+// export function createGetter(path) {
+//   return createGetter1(path);
+// }
+
+// 1st implementation (iteration)
+function createGetter1(path) {
+  const objectKeysPath = path.split('.');
+
+  return obj => {
+    let result = obj;
+
+    for (let key of objectKeysPath) {
+      if (!result.hasOwnProperty(key)) {
+        return;
+      }
+
+      result = result[key];
+    }
+
+    return result;
+  };
+}
+
+// 2nd implementation (recursion)
+function createGetter2(path) {
+  const objectKeysPath = path.split('.');
+
+  const getPropertyValue = obj => {
+    const currentKey = objectKeysPath.shift();
+    const isLastKey = objectKeysPath.length === 0;
+
+    if (!obj.hasOwnProperty(currentKey)) {
+      return;
+    }
+
+    const lastValue = obj[currentKey];
+    if (isLastKey) {
+      return lastValue;
+    }
+
+    return getPropertyValue(lastValue);
+  };
+
+  return obj => {
+    return getPropertyValue(obj);
+  };
+}
+
+// 3rd implementation (reduce)
+function createGetter3(path) {
 
 }
+
+
+const product = {
+  category: {
+    title: "Goods"
+  }
+};
+
+const getter = createGetter3('category.title');
+
+console.log(getter(product)); // Goods
