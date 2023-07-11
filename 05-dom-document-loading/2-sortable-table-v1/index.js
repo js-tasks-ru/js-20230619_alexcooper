47c362411824d094dc65d98aaaa6c6ecab330fc0
Alex Cooper <616126+bondarev@users.noreply.github.com>
2023-07-11 17:34:20 +0400

    feat(SortableTable): add `SortableTable` component

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,6 +1,152 @@
-export default class SortableTable {
-  constructor(headerConfig = [], data = []) {
+import createElementFromString from '../../lib/create-element.js';
+import { compareFunction as compareStrings } from '../../lib/sort-strings.js';
 
-  }
+function compareNumbers(a, b) {
+  return a - b;
 }
 
+export default class SortableTable {
+  element;
+  subElements = {};
+  sortedColumnElement;
+
+  constructor(headerConfig = [], data = []) {
+    this.headerConfig = headerConfig;
+    this.data = data;
+
+    this.#render();
+  }
+
+  get #template() {
+    return `
+      <div class="sortable-table">
+        ${this.#getTableHeaderHTML()}
+        ${this.#getTableBodyHTML()}
+      </div>
+    `;
+  }
+
+  #getTableHeaderHTML() {
+    const tableRowsHTML = this.headerConfig
+      .map(options => this.#getTableHeaderRowHTML(options))
+      .join('');
+
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${tableRowsHTML}
+      </div>
+    `;
+  }
+
+  #getTableHeaderRowHTML({id, title, sortable}) {
+    return `
+      <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+        <span>${title}</span>
+        <span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>
+      </div>
+    `;
+  }
+
+  #getTableBodyHTML() {
+    return `
+      <div data-element="body" class="sortable-table__body">
+        ${this.#getTableBodyRowsHTML()}
+      </div>
+    `;
+  }
+
+  #getTableBodyRowsHTML(data = this.data) {
+    return data.map(rowData => {
+      return `
+        <a href="/products/${rowData.id}" class="sortable-table__row">
+            ${this.#getTableBodyRowHTML(rowData)}
+        </a>
+      `;
+    }).join('');
+  }
+
+  #getTableBodyRowHTML(rowData) {
+    return this.headerConfig.map(({id, template}) => {
+      const value = rowData[id];
+
+      if (template) {
+        return template(value);
+      }
+
+      return `<div class="sortable-table__cell">${value}</div>`;
+    }).join('');
+  }
+
+  #render() {
+    this.element = createElementFromString(this.#template);
+    this.subElements = this.#getSubElements(this.element);
+  }
+
+  #getSubElements(element) {
+    const subElements = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    elements.forEach(element => {
+      const elementName = element.dataset.element;
+
+      subElements[elementName] = element;
+    });
+
+    return subElements;
+  }
+
+  #remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  #sortData(field, order) {
+    const sortedData = [...this.data];
+    const direction = order === 'asc' ? 1 : -1;
+
+    const column = this.headerConfig.find(column => column.id === field);
+    const { sortable, sortType } = column;
+
+    if (!sortable) {
+      return sortedData;
+    }
+
+    sortedData.sort((a, b) => {
+      const aValue = a[field];
+      const bValue = b[field];
+
+      switch (sortType) {
+      case 'number':
+        return direction * compareNumbers(aValue, bValue);
+      case 'string':
+        return direction * compareStrings(aValue, bValue);
+      default:
+        throw new Error(`Unsupported sort type "{${sortType}"`);
+      }
+    });
+
+    return sortedData;
+  }
+
+  sort(field, order = 'asc') {
+    const sortedData = this.#sortData(field, order);
+    const sortedColumnElement = this.subElements.header.querySelector(`[data-id="${field}"]`);
+
+    if (this.sortedColumnElement) {
+      delete this.sortedColumnElement.dataset.order;
+    }
+
+    sortedColumnElement.dataset.order = order;
+    this.sortedColumnElement = sortedColumnElement;
+
+    this.subElements.body.innerHTML = this.#getTableBodyRowsHTML(sortedData);
+  }
+
+  destroy() {
+    this.#remove();
+    this.data = null;
+  }
+}
